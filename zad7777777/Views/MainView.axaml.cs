4ca1a4ea116beed9b0efee8be23d883583d1b603
szymon.m.olejniczak <szymon.m.olejniczak@student.put.poznan.pl>
2025-04-18 16:25:05 +0200

    najtrudniesza częsc to srolowanie xddddddd z 1h szukalem rozwiazania :(

--- zad7777777/Views/MainView.axaml.cs
+++ zad7777777/Views/MainView.axaml.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using System;
 using System.Collections.Generic;
 
 namespace zad7777777.Views;
@@ -12,25 +14,48 @@ public partial class MainView : UserControl
     }
     int i = 0;
 
+    public event EventHandler<ScrollChangedEventArgs> ScrollHorizontalScroll;
+
     public void Button_OnClick(object? sender, RoutedEventArgs e)
     {
         string rnaseq;
         string temprna;
+        Dictionary<string, int> openWith = new Dictionary<string, int>();
         if (rna.Text != null)
         {
+            
             rnaseq = rna.Text;
-            List<string> list = new List<string>(); 
+           // List<string> list = new List<string>(); 
             for (i = 0; i < rnaseq.Length-4; i++)
             {
                 temprna = rnaseq.Substring(i, 4);
                 //list.Find(temprna);
-                list.Add(temprna);
-                
+                try
+                {
+                    openWith.Add(temprna, 1);
+                }
+                catch (ArgumentException)
+                {
+                    openWith[temprna] = openWith[temprna]+1;
+                }
+
+                //list.Add(temprna);
+
             }
         }
-       
-        wynik.Text = i++.ToString();    
-      
-       
+        string tempo = "";
+        foreach (KeyValuePair<string, int> item in openWith)
+        {
+            wynik.Text += item.Key + " " + item.Value + "\n";
+        }
+        
+
+
+       // ScrollViewer.IsScrollChainingEnabled = [true | false]
+       //wynik.Text = i++.ToString();    
+
+
     }
+    public string text = "";
+
 }
